949200fe15bc9988499cc6fe3e9cc32f60bb0a55
Mostafa Shalaby <74937937+mostafashalaby@users.noreply.github.com>
2024-11-13 10:23:23 -0800

    fixed encoding

--- datascraper.py
+++ datascraper.py
@@ -9,6 +9,7 @@ import re
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
+sys.stdout.reconfigure(encoding='utf-8') # Set the standard output encoding to UTF-8
 load_dotenv() # Load environment variables from .env file
 
 SPOTIPY_CLIENT_ID = ''
@@ -30,6 +31,19 @@ def init():
 
     return sp, genius
 
+def read_from_csv(csv_file, songs_data):
+    # Check if the file exists before attempting to open it
+    if not os.path.exists(csv_file):
+        print(f"File '{csv_file}' does not exist. Skipping read operation.")
+        return
+
+    with open(csv_file, mode='r', encoding="utf-8") as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Skip the header row
+        for row in csv_reader:
+            artist_name, track_name, genre, lyrics = row
+            songs_data[(artist_name, track_name)] = [genre, lyrics]
+
 def get_playlist_artist_and_track(sp, playlist_id, genre, songs_data):
     playlist = sp.playlist(playlist_id)
     tracks = playlist['tracks']['items']
@@ -49,30 +63,29 @@ def get_playlist_artist_and_track(sp, playlist_id, genre, songs_data):
 def get_lyrics_category(genius, artist_track_list, genre, songs_data):
     lyrics = {}
     for artist, track in artist_track_list:
-        try:
-            time.sleep(1) # Sleep for 1 second to avoid rate limiting
-            song = genius.search_song(track, artist)
-            if song.lyrics:
-                track_lyrics = song.lyrics
-                print("Lyrics: ", song.lyrics)
-            else:
-                print(f"Lyrics not found for song: {track} by {artist}. Skipping...\n")
-                track_lyrics = None
-
+        time.sleep(1) # Sleep for 1 second to avoid rate limiting
+        song = genius.search_song(track, artist)
+        if song.lyrics:
+            track_lyrics = song.lyrics.encode('utf-8', 'ignore').decode('utf-8')
+            print("Lyrics: ", track_lyrics)
+        else:
+            print(f"Lyrics not found for song: {track} by {artist}. Skipping...\n")
+            track_lyrics = None
+        """
         except Exception as e:
             print(f"An error occurred for song: {track} by {artist}: {e}. Skipping...\n")
             track_lyrics = None
-            break
+            break"""
 
         # Update the lyrics dictionary and songs_data dictionary with the lyrics
-        lyrics[(artist, track)] = track_lyrics
+        lyrics[(artist, track)] = track_lyrics.strip()
         songs_data[(artist, track)][0] = genre
         songs_data[(artist, track)][1] = track_lyrics
 
     return lyrics
 
 def write_to_csv(songs_data, csv_file):
-    with open(csv_file, mode='a', encoding="utf-8") as file:
+    with open(csv_file, mode='a', encoding="utf-8", newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['artist_name', 'track_name', 'genre', 'lyrics'])
         for (artist, track), [genre, lyrics] in songs_data.items():
@@ -97,6 +110,13 @@ if __name__ == "__main__":
     lyrics_by_genre = {} # format: {genre: [(artist_name, track_name), lyrics]}
     songs_data = {} # format: {(artist_name, track_name): [genre, lyrics]}
 
+    # read from csv file first, to only add entried that do not have lyrics
+    read_from_csv(csv_file, songs_data)
+    #for key, value in songs_data.items():
+    #    if value[1] is None:
+    #        print(key, value)
+
+    
     for genre, playlist_id in genres.items():
         tracks = get_playlist_artist_and_track(sp, playlist_id, genre, songs_data)
         lyrics = get_lyrics_category(genius, tracks, genre, songs_data)
